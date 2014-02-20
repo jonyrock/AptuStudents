e3dcd76ff3bb5c9f2a37f4f247fb34c9b43c69e9
jonyrock <ilms@live.ru>
2014-02-20 13:15:12 +0400

    init commit

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/home/alex/Sites/AptuStudents/conf/routes
+// @HASH:5167628e0f508914f83f02656f97c5de1eec3bcc
+// @DATE:Thu Feb 20 13:14:18 MSK 2014
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+}
+          
