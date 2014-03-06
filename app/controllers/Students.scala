1ca55a35d9d1d727bd5755b7eb63e8d048853ba1
jonyrock <ilms@live.ru>
2014-03-06 16:11:37 +0400

    moskow code

--- app/controllers/Students.scala
+++ app/controllers/Students.scala
@@ -1,12 +1,30 @@
 package controllers
 
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import models.Student
+import views._
 
 object Students extends Controller {
   
+  val studentForm = Form(
+    mapping(
+      "name"-> nonEmptyText(minLength = 5, maxLength = 40),
+      "department"-> text
+    )(Student.apply)(Student.unapply)
+  )
+  
   def list = TODO
+  
+  def create = Action { request =>
+    Ok(html.createResult("Success"))
+  }
 
-  def create = TODO
+  def createForm = Action {
+    Ok(html.createForm(studentForm))
+  }
   
   def show(id: Int) = TODO
   
